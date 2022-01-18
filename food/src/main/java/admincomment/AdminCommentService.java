eb82259c386d09712f5314bde4c0f6a400c5178c
user <user@DESKTOP-MF6GKVI>
2022-01-18 16:02:45 +0900

    앙 삭제까지 끝 기모링 최재훈

--- food/src/main/java/admincomment/AdminCommentService.java
+++ food/src/main/java/admincomment/AdminCommentService.java
@@ -5,6 +5,8 @@ import java.util.List;
 public interface AdminCommentService {
 
 	public List<AdminCommentVo> selectList(AdminCommentVo vo);
-	public int admincommentDelete(String adc_no);
 	public int count(AdminCommentVo vo);
+	public int delete(AdminCommentVo vo);
+	public int adcdelete(String adc_no);
+	
 }
