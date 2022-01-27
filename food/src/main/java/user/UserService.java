df398e89fc8a7f5b51abd4aaf3fbb77c96e1a3d2
jeonghyeonsu <jeonghyeonsu@localhost>
2022-01-27 15:04:36 +0900

    현수 로그인 기능구현asdasda

---
+++ food/src/main/java/user/UserService.java
@@ -0,0 +1,22 @@
+package user;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+	@Autowired
+	UserDao dao;
+	
+	public boolean login(UserVo vo, HttpSession sess) {
+		UserVo uv = dao.login(vo);
+		if (uv != null) { // 로그인 성공
+			sess.setAttribute("adminInfo", uv);
+			return true;
+		}
+		return false;
+	}
+}
